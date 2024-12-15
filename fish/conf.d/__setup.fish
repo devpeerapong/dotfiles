c8477bc013b14ae4063e2a59f058c1a1a160809e
Field <me@peerapong.dev>
2024-12-16 02:57:01 +0700

    fix fish setup script infinite loop

--- fish/conf.d/__setup.fish
+++ fish/conf.d/__setup.fish
@@ -2,29 +2,64 @@ if [ (uname -m) = arm64 ]
     eval "$(/opt/homebrew/bin/brew shellenv)"
 end
 if [ (uname -m) = x86_64 ]
-    eval "$(/usr/local/bin/brew shellenv)"
+		if test -f /usr/local/bin/brew
+        eval "$(/usr/local/bin/brew shellenv)"
+    else if test -f /home/linuxbrew/.linuxbrew/bin/brew
+        eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    else
+			echo 'brew bin not found'
+			exit 1
+    end
 end
 
-set -l list \
-    'fd			brew install fd' \
-    'eza		brew install eza' \
-    'rg			brew install ripgrep' \
-    'bat		brew install bat' \
-    'fzf		brew install fzf' \
-    'zoxide		brew install zoxide' \
-    'lazygit	brew install lazygit' \
-    'delta		brew install git-delta' \
-    'goku		brew install yqrashawn/goku/goku' \
-    'fnm		/bin/bash -c "$(curl -fsSL https://fnm.vercel.app/install)"' \
-    'starship	/bin/bash -c "$(curl -fsSL https://starship.rs/install.sh)"' \
-    'fisher		curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher && fisher update'
+set -l brewcli fd eza bat fzf zoxide lazygit
+for item in $brewcli
+    if not type -q $item
+        echo Installing $item
+        eval "brew install $item"
+        echo Done $item
+    end
+end
 
-for item in $list
-    set -l line (string split -m 1 \t $item)
+set -l brewaliascli \
+		'rg repgrep' \
+		'delta gitdelta' 
+
+	
+for item in $brewaliascli
+    set -l line (string split " " $item)
 
     if not type -q $line[1]
         echo Installing $line[1]
-        eval (string trim $line[2])
+        eval "brew install (string trim $line[2])"
         echo Done $line[1]
     end
+end
+
+set -l cli \
+		'fnm' \
+		'starship' \
+		'fisher'
+
+set -l clitest \
+		'test -d ~/.local/share/fnm' \
+		'type -q starship' \
+		'test -f $__fish_config_dir/functions/fisher.fish'
+
+set -l cliinstall \
+		'curl -fsSL https://fnm.vercel.app/install | bash' \
+		'/bin/bash -c "$(curl -fsSL https://starship.rs/install.sh)"' \
+		'curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher && fisher update'
+
+set index 1
+for item in $cli
+	eval $clitest[$index]
+
+	if test ! $status -eq 0
+			echo Installing $item
+			eval $cliinstall[$index]
+			echo Done $item
+	end
+
+	set index (math $index + 1)
 end
